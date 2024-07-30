23e58543242efdd2a79c8ba6ad141ec844619686
Chen Chu <chenchu1997@gmail.com>
2024-07-29 17:04:00 -0700

    test github pages

---
+++ src/AWSBadge.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AWSBadge() {
+    <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+    return (
+        <>
+             <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="33555366-92aa-4dc8-aff0-cd7870bd2866" data-share-badge-host="https://www.credly.com"></div>
+             
+        </>
+    )
+}
+
+export default AWSBadge;
---
+++ src/GoalList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function GoalList() {
+    return (
+        <>
+          <ul className="goal-list">
+            <li>Cloud Certificate</li>
+            <li>React Tutorial</li>
+            <li>Express Tutorial</li>
+          </ul>
+        </>
+      )
+}
+
+export default GoalList;
